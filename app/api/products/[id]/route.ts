aaaca315f997b66feb6ea6b8188a3af1e90890d8
christianLudovic <talchristgmail.com>
2025-03-04 19:46:25 +0300

    test 3

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -3,9 +3,8 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(
-  request: Request, // Use `Request` or `NextRequest` from 'next/server'
-  { params }: { params: {id}} // Destructure `params` directly
+export async function GET({ params }: { params: {id : string} },
+  request: Request
 ) {
   try {
     const product = await prisma.product.findUnique({
